318b7595b43e19665bb2d7c5e3c57a273894d5cb
AlejandroMoralBermejo <alejandromoralbermejo@gmail.com>
2025-03-25 10:20:23 +0100

    innit commit

---
+++ geolocalizador/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import HeaderPagina from './componentes/HeaderPagina';
+import BodyPagina from './componentes/BodyPagina';
+
+
+function App() {
+  return (
+    <div >
+
+    </div>
+  );
+}
+
+export default App;
